3cc9166f948d168a25b709079f08865216f1683d
George Paton <georgewpaton@gmail.com>
2016-02-10 22:58:39 +1100

    holy shit this is perf

--- js/custom.js
+++ js/custom.js
@@ -4,17 +4,23 @@ function scrollSmoothTo(element) {
 	}, 1000);
 }
 
+function setImgScroll() {
+     $(".img-wrapper img").each(function (e) {
+        var bodyScrollTop = $("body").scrollTop(),
+            imgOffsetTop = $(this).parent().offset().top,
+            scrollHeightOfElement = ((bodyScrollTop - imgOffsetTop) * 0.7) - ($(window).height() / 5) + (imgOffsetTop / 20);
+        
+        $(this).css("top", scrollHeightOfElement);
+        console.log(scrollHeightOfElement);
+    })
+}
+
 if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
     $(".header").css("margin-top", -70)
 }
 
 $(window).scroll(function () {
-    $(".img-wrapper img").each(function (e) {
-        var bodyScrollTop = $("body").scrollTop(),
-            imgOffsetTop = $(this).parent().offset().top,
-            scrollHeightOfElement = ((bodyScrollTop - imgOffsetTop) * 1.3) - ($(window).height() / 6);
-        
-        $(this).css("top", scrollHeightOfElement);
-        console.log(scrollHeightOfElement);
-    })
+    setImgScroll();
 })
+
+setImgScroll();
