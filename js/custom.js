1fc5f2c23fc66594f9fa7a7fd712d148b3714a6d
George Paton <georgewpaton@me.com>
2017-05-11 12:21:03 +1000

    Reimplement proper browser behaviour in scroll smooth to

--- js/custom.js
+++ js/custom.js
@@ -6,16 +6,8 @@ var currentScrollTop = $(window).scrollTop(),
     // We keep a reference to the length of the array to prevent extra array accesses
     allImagesLength = allImages.length,
     downArrow = $(".chevron-down"),
-    scrollCoefficient = 2.2;
-
-/** Scroll to any defined element over one second **/
-function scrollSmoothTo(element) {
-    'use strict';
-
-    $("html, body").animate({
-        scrollTop: $(element).offset().top
-    }, 1000);
-}
+    scrollCoefficient = 2.2,
+    scrollTime = 1000;
 
 /** Record the image offset as a data attribute in the image **/
 function getImageOffsets() {
@@ -78,8 +70,38 @@ function init() {
     // Begin the animation loop
     requestAnimationFrame(animLoop);
 
-    // Set image offsets
+    // Get image y offset from top of screen
     getImageOffsets();
+    
+    // Get all non empty hash links and attach scroll
+    $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function (event) {
+        console.log("click!");
+        if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') &&
+                location.hostname === this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                // Prevent default browser behaviour
+                event.preventDefault();
+                location.hash = this.hash;
+                // Scroll for scrollTime to the marker
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, scrollTime, function () {
+                    // After animation finishes, set focus
+                    // Reimplements default browser behaviour
+                    var $target = $(target);
+                    $target.focus();
+                    if ($target.is(":focus")) {
+                        return false;
+                    } else {
+                        $target.attr('tabindex', '-1');
+                        $target.focus(); // Re set the focus
+                    }
+                });
+            }
+        }
+    });
 }
 
 /** Fix up the location of the content arrow on iOS Safari.
@@ -127,4 +149,5 @@ window.addEventListener("mousewheel", function () {
 });
 
 // Now we start the execution
+// Since we tail load the JS, we don't have to wait for the page
 init();
