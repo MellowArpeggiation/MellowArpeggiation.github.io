dfd4cfd2024c4180509d2487076250269233e04f
George Paton <georgewpaton@gmail.com>
2017-06-24 17:50:32 +1000

    Better, more readable string creation

--- js/glitch.js
+++ js/glitch.js
@@ -1,29 +1,26 @@
-/*global $*/
-
 function swapElegant(element) {
     'use strict';
-    // This function changes the letters in "Elegant"
-
-    var createdString = "";
+    // This function changes the letters in 'Elegant'
 
     // Easier to end the function than to vary interval length
     if (Math.random() > 0.08) {
         return;
     }
 
-    // Short form conditionals used for readability
     // Adds a random combination of 313 and Ele to the string
-    createdString += (Math.random() > 0.5 ? "E" : "3");
-    createdString += (Math.random() > 0.5 ? "l" : "1");
-    createdString += (Math.random() > 0.5 ? "e" : "3");
-    createdString += "gant";
+    var createdString = [
+        Math.random() > 0.5 ? 'E' : '3',
+        Math.random() > 0.5 ? 'l' : '1',
+        Math.random() > 0.5 ? 'e' : '3',
+        'gant'
+    ].join('');
 
     // Set html and data-text, so CSS based glitching works too
     $(element).html(createdString);
-    $(element).attr("data-text", createdString);
+    $(element).attr('data-text', createdString);
 }
 
-$(".glitch").each(function () {
+$('.glitch').each(function () {
     'use strict';
 
     setInterval(swapElegant, 50, this);
