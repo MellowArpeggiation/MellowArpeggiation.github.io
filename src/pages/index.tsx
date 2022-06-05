a972a1a78606d6b80691845da8ec549f4684679c
George Paton <georgewpaton@me.com>
2022-06-05 15:53:57 +1000

    Add some parallax effects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
 import "../styles/main.scss"
 import Header from "../components/header"
 // @ts-ignore
@@ -7,11 +8,19 @@ import AboutMdx from "../sections/about"
 
 // markup
 const IndexPage = () => {
+    const parallax = React.useRef<IParallax>(null!)
+
     return (
         <main>
             <title>Mellow Portfolio</title>
-            <Header title="Mellow" />
-            <AboutMdx />
+            <Parallax ref={parallax} pages={2}>
+                <ParallaxLayer speed={1}>
+                    <Header title="Mellow" />
+                </ParallaxLayer>
+                <ParallaxLayer offset={1}>
+                    <AboutMdx />
+                </ParallaxLayer>
+            </Parallax>
         </main>
     )
 }
