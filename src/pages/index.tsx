8105751b756b0363cd819e9111af846b62955f21
George Paton <georgewpaton@me.com>
2022-06-06 14:56:49 +1000

    Add anagram to title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Parallax, ParallaxLayer, IParallax } from '@react-spring/parallax'
+import Anagram from 'react-anagram-animation'
 import "../styles/main.scss"
 import Header from "../components/header"
 // @ts-ignore
@@ -14,10 +15,30 @@ const IndexPage = () => {
         <main>
             <title>Mellow Portfolio</title>
             <Parallax ref={parallax} pages={3}>
+
+                {/* Title area */}
                 <ParallaxLayer className="page" speed={1}>
                     <Header title="Mellow" />
                     <div className="scroll-indicator"></div>
                 </ParallaxLayer>
+                <ParallaxLayer className="page" offset={0.15} speed={1.1}>
+                    <div className="anagram-container">
+                        <span className="text-small">&lt;by&gt;</span>
+                        <Anagram
+                            words={['mellow arpeggiation', 'george william paton']}
+                            animationOptions={{
+                                randomStartMin: 0,
+                                randomStartMax: 1000,
+                                randomReverseMin: 6000,
+                                randomReverseMax: 7000,
+                                loopAnimation: 12000,
+                                waitToStart: 8000,
+                            }}
+                        />
+                    </div>
+                </ParallaxLayer>
+
+                {/* About section */}
                 <ParallaxLayer className="background-title" offset={0.9} speed={0.25}>
                     <div className="up-down">
                         ABOUT
@@ -27,6 +48,13 @@ const IndexPage = () => {
                     <section>
                         <AboutMdx />
                     </section>
+                </ParallaxLayer>
+
+                {/* Project section */}
+                <ParallaxLayer className="background-title" offset={1.6} speed={0.25}>
+                    <div className="up-down">
+                        PROJECT
+                    </div>
                 </ParallaxLayer>
             </Parallax>
         </main>
