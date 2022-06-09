900e257af625bedc2218890495279b61137435d0
George Paton <georgep@webforcefive.com.au>
2022-06-09 16:31:13 +1000

    Add links to all projects

--- src/sections/projects.tsx
+++ src/sections/projects.tsx
@@ -33,68 +33,88 @@ import ProtohookMdx from "../sections/bachelor/protohook"
 type Project = {
     content: HTMLElement,
     date: string,
+    color: string,
 }
+
+const personalColor = 'magenta'
+const uniColor = 'yellow'
+const workColor = 'red'
 
 const Projects = [
     {
         content: DriftKingMdx,
         date: '2020 - now',
+        color: personalColor,
     },
     {
         content: BarefootBowenMdx,
         date: '2022',
+        color: personalColor,
     },
     {
         content: TotemMdx,
         date: '2021',
+        color: personalColor,
     },
     {
         content: LoccysChoccysMdx,
         date: '2021',
+        color: personalColor,
     },
     {
         content: OkBoomerMdx,
         date: '2019 - 2020',
+        color: personalColor,
     },
     {
         content: MortalisMachinaMdx,
         date: '2018 - on hold',
+        color: personalColor,
     },
     {
         content: WebForceFiveMdx,
         date: '2016',
+        color: workColor,
     },
     {
         content: MovieJazzMdx,
         date: '2016',
+        color: workColor,
     },
     {
         content: GraduatedMdx,
         date: '2016',
+        color: uniColor,
     },
     {
         content: GreentreeMdx,
         date: '2015',
+        color: workColor,
     },
     {
         content: PointourMdx,
         date: '2015',
+        color: uniColor,
     },
     {
         content: FlubaMdx,
         date: '2014',
+        color: uniColor,
     },
     {
         content: EatElegantMdx,
         date: '2014',
+        color: uniColor,
     },
     {
         content: ProtohookMdx,
         date: '2013',
+        color: uniColor,
     },
     {
         content: OceaneeringMdx,
         date: '2011',
+        color: workColor,
     },
 ]
 
